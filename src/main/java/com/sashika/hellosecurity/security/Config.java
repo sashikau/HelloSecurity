2c6ebfb1fe84970bc4c5a293a3391a812bcd8db7
sashikau <hasudana@gmail.com>
2023-03-09 17:01:12 +0530

    moved configuration class to security package

--- src/main/java/com/sashika/hellosecurity/security/Config.java
+++ src/main/java/com/sashika/hellosecurity/security/Config.java
@@ -1,4 +1,4 @@
-package com.sashika.hellosecurity;
+package com.sashika.hellosecurity.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
