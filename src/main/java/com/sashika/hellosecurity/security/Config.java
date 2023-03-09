1a7573b2587ed2a22823ccfd15873bad6cddd655
sashikau <hasudana@gmail.com>
2023-03-09 17:31:01 +0530

    introducing custom AuthenticationProvider impl and using it in WebSecurityConfigurerAdapter impl

--- src/main/java/com/sashika/hellosecurity/security/Config.java
+++ src/main/java/com/sashika/hellosecurity/security/Config.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 /**
  * Created by Sashika.Udana
@@ -12,29 +11,17 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
  */
 @Configuration
 public class Config extends WebSecurityConfigurerAdapter {
-   /** @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-        userDetailsService.createUser(User.withUsername("John").password("123456").authorities("read").build());
 
-        return userDetailsService;
+    private final CustomAuthenticationProvider authenticationProvider;
+
+    public Config(CustomAuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();// treats password as plain text
-    }**/
-
-   //Instead of defining beans separately (as in above) we can do it via below method
-   @Override
-   protected void configure(AuthenticationManagerBuilder auth)throws Exception {
-       auth.inMemoryAuthentication()
-               .withUser("John")
-               .password("123456")
-               .authorities("read")
-               .and()
-               .passwordEncoder(NoOpPasswordEncoder.getInstance());
-   }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
---
+++ src/main/java/com/sashika/hellosecurity/security/CustomAuthenticationProvider.java
@@ -0,0 +1,35 @@
+package com.sashika.hellosecurity.security;
+
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * Created by Sashika.Udana
+ * on 09/Mar/2023
+ */
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+        if ("john".equals(username) && "123456".equals(password)) {
+            return new UsernamePasswordAuthenticationToken(username, password, Arrays.asList());
+        }
+        else {
+            throw new AuthenticationCredentialsNotFoundException("Error!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
