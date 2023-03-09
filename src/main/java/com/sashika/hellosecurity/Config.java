4207bb9abf0f941d0cd4820a7e212a134497faae
sashikau <hasudana@gmail.com>
2023-03-09 16:35:32 +0530

    Update HttpSecurity configuration to allow unrestricted access to all requests

--- src/main/java/com/sashika/hellosecurity/Config.java
+++ src/main/java/com/sashika/hellosecurity/Config.java
@@ -2,6 +2,8 @@ package com.sashika.hellosecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -13,7 +15,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
  * on 09/Mar/2023
  */
 @Configuration
-public class Config {
+public class Config extends WebSecurityConfigurerAdapter {
     @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
@@ -25,5 +27,12 @@ public class Config {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();// treats password as plain text
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests()
+                .anyRequest().permitAll();
     }
 }
